10194c63d3a1f6aeba53617a7503248e02cdc06b
Diego Riano <diriano@gmail.com>
2023-09-27 12:41:20 -0300

    adding code to update TOC

---
+++ TOC/create_TOC.sh
@@ -0,0 +1,10 @@
+
+# rerun if any changes are made to the # ## ### #### in the README.md
+head -n 11 ../bioinfo.md > toc.md
+START=2
+tail +$START ../unix.md | ./gh-md-toc - | perl -p -e 's/\(#/\(unix.md\/#/' >> toc.md
+#tail +$START ../pfb.md  | ./gh-md-toc - | perl -p -e 's/\(#/\(pfb.md\/#/' >> toc.md
+#tail +$START ../workshops/README.md | ./gh-md-toc - | perl -p -e 's/\(#/\(workshops.md\/#/' >> toc.md
+
+
+## copy the contents of toc.md to README.md to create a TOC with more depth
