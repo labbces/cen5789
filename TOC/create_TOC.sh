bfd0fdff2dd4e96f896e16d179e3ee15cfdc8d9f
Diego Riano <diriano@gmail.com>
2023-09-29 17:10:58 -0300

    adding bioinfo, not working yet

--- TOC/create_TOC.sh
+++ TOC/create_TOC.sh
@@ -1,9 +1,9 @@
 
 # rerun if any changes are made to the # ## ### #### in the README.md
-head -n 11 ../bioinfo.md > toc.md
+head -n 11 ../unix.md > toc.md
 START=2
-tail +$START ../unix.md | ./gh-md-toc - | perl -p -e 's/\(#/\(unix.md\/#/' >> toc.md
-#tail +$START ../pfb.md  | ./gh-md-toc - | perl -p -e 's/\(#/\(pfb.md\/#/' >> toc.md
+tail +10 ../unix.md | ./gh-md-toc - | perl -p -e 's/\(#/\(unix.md\/#/' >> toc.md
+tail +6 ../bioinfo.md  | ./gh-md-toc - | perl -p -e 's/\(#/\(pfb.md\/#/' >> toc.md
 #tail +$START ../workshops/README.md | ./gh-md-toc - | perl -p -e 's/\(#/\(workshops.md\/#/' >> toc.md
 
 
