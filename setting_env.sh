6a8c5b983010e54f4de0153f97eda2d3a7e1f772
Diego Mauricio Riaño-Pachón <diriano@users.noreply.github.com>
2023-11-07 16:43:15 -0300

    Update setting_env.sh

--- setting_env.sh
+++ setting_env.sh
@@ -73,3 +73,7 @@ sudo apt install --no-install-recommends software-properties-common dirmngr
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 sudo apt install --no-install-recommends r-base
+
+#cd ~/Downloads; wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2023.09.1-494-amd64-debian.tar.gz
+#tar xvzf rstudio-2023.09.1-494-amd64-debian.tar.gz
+#sudo mv rstudio-2023.09.1+494/ /usr/local
