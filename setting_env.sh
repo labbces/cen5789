97423b6a70685cf8a3917dcb043329ad7b6fe3c9
Diego Mauricio Riaño-Pachón <diriano@users.noreply.github.com>
2024-07-15 15:13:48 -0300

    Update setting_env.sh

--- setting_env.sh
+++ setting_env.sh
@@ -16,6 +16,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
+. /home/cen5789/.bashrc
 
 #restart computer and then install conda software
 
