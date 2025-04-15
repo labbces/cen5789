971f143798b5a5eab7e10bf259c3662ce2d6a749
Diego Mauricio Riaño-Pachón <diriano@users.noreply.github.com>
2025-04-15 14:59:59 -0300

    Update setting_env.sh

--- setting_env.sh
+++ setting_env.sh
@@ -60,7 +60,7 @@ conda create -y -n tidk -c bioconda tidk
 conda create -y -n compleasm -c conda-forge -c bioconda compleasm
 conda create -y -n merqury -c bioconda merqury
 
-conda create -y -n jupiterplot -c bioconda circos samtools minimap2
+conda create -y -n jupiterplot -c bioconda circos samtools minimap2 circos-tools
 
 conda create -y -n singularitycew -c wallaulab singularityce conda-forge::cni-plugins
 ln -s /usr/bin/mksquashfs /usr/local/bin/
